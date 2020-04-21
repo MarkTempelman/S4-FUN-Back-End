d4570c6b2434f40044e62952e4d3ac86ec0db3b3
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-04-21 10:23:23 +0200

    Added web and security config

--- weekmenu/src/main/java/com/weekmenu/weekmenu/WeekmenuApplication.java
+++ weekmenu/src/main/java/com/weekmenu/weekmenu/WeekmenuApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @SpringBootApplication
 public class WeekmenuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeekmenuApplication.class, args);
     }
+
 }
